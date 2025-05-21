a651b51433349de7ae82545ebb04a56ed56a7d12
yasir-nizame <ynizami72@gmail.com>
2025-05-21 20:35:26 +0500

    Added / endpoint

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "Hello from the Confizio Server!",
+  });
+});
+
 // API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/author", authorRoute);
@@ -34,13 +41,6 @@ app.use("/api/reviewer", reviewerRoute);
 // Handle unknown API routes properly
 app.all("/api/*", (req, res) => {
   res.status(404).json({ message: "API route not found" });
-});
-
-// Serve React frontend
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 
 // Start Server
